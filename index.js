e1b214cf14ebe073f77134ff4fcfd849c15c1e01
Gianni Utoro <53757768+Gianni2U@users.noreply.github.com>
2022-02-08 19:14:17 +0100

    idk

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const app = express();
 var cors = require('cors');
 
 app.use(
-    cors({
-        credentials: true,
-        origin: 'https://loving-hodgkin-f2e7ae.netlify.app/'
-    })
-    app.get('/products/:id', function (req, res, next) {
+    cors())
+
+    app.get('/', function (req, res, next) {
       res.json({msg: 'This is CORS-enabled for all origins!'})
 );
 app.options('*', cors());
