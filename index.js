3f83cb8da56d079685b5777559872bd45234b80a
Gianni Utoro <53757768+Gianni2U@users.noreply.github.com>
2022-02-08 19:25:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const app = express();
 var cors = require('cors');
 
 app.use(
-    cors({
-        credentials: true,
-        origin: 'https://loving-hodgkin-f2e7ae.netlify.app/'
-    })
-);
-app.options('*', cors());
+    cors());
 
-app.get('/', (req, res) => res.send('This api works.'));
+app.get('/', cors(), (req, res) => res.send('This api works.'));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
